cb6b59dedcbef9ff346ecf48c99e24089084e98e
Cletus <cletus6@gmail.com>
2021-06-25 12:30:02 -0400

    create add product mutation

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,5 +1,32 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
+import DisplayError from './ErrorMessage';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    $name: String
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, resetForm, clearForm } = useForm({
@@ -8,15 +35,26 @@ export default function CreateProduct() {
     price: 199,
     description: 'Nice shoes',
   });
+
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
+
   return (
     <div>
       <Form
-        onSubmit={(e) => {
+        onSubmit={async (e) => {
           e.preventDefault();
-          console.log(inputs);
+          const res = await createProduct();
+          clearForm();
+          console.log(res);
         }}
       >
-        <fieldset>
+        <DisplayError error={error} />
+        <fieldset disabled={loading} aria-busy={loading}>
           <label htmlFor="image">
             Image
             <input
--- frontend/lib/useForm.js
+++ frontend/lib/useForm.js
@@ -1,4 +1,3 @@
-import { func } from 'prop-types';
 import { useState } from 'react';
 
 export default function useForm(initialState = {}) {
