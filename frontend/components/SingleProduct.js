a408e80f3f3e989e56044ab9cfb7472a724b9386
Cletus <cletus6@gmail.com>
2021-08-30 22:51:59 -0400

    add styles to single product

--- frontend/components/SingleProduct.js
+++ frontend/components/SingleProduct.js
@@ -1,11 +1,32 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
+import Head from 'next/head';
+import styled from 'styled-components';
 import DisplayError from './ErrorMessage';
+
+const ProductStyles = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  max-width: var(--maxWidth);
+  img {
+    width: 100%;
+    object-fit: contain;
+  }
+  align-items: top;
+  gap: 2rem;
+`;
 
 const SINGLE_ITEM_QUERY = gql`
   query SINGLE_ITEM_QUERY($id: ID!) {
     Product(where: { id: $id }) {
       name
+      photo {
+        image {
+          publicUrlTransformed
+        }
+        altText
+      }
       description
       price
     }
@@ -18,10 +39,20 @@ export default function SingleProduct({ id }) {
   });
   if (loading) return <p>Loading...</p>;
   if (error) return <DisplayError error={error} />;
-  console.log({ data, loading, error });
+  //   console.log({ data, loading, error });
+  const { Product } = data;
   return (
-    <div>
-      <h2>{data.Product.name}</h2>
-    </div>
+    <ProductStyles>
+      <Head>
+        <title>Fits | {Product.name}</title>
+      </Head>
+      <img
+        src={Product.photo.image.publicUrlTransformed}
+        alt={Product.photo.altText}
+      />
+      <div className="details">
+        <h2>{Product.name}</h2>
+      </div>
+    </ProductStyles>
   );
 }
