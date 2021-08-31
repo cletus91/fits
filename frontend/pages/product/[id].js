d99f9c336be77deba9e1786d4081fd579ab3a116
Cletus <cletus6@gmail.com>
2021-08-30 21:46:12 -0400

    seperate single product component from page

--- frontend/pages/product/[id].js
+++ frontend/pages/product/[id].js
@@ -1,27 +1,5 @@
-import { useQuery } from '@apollo/client';
-import gql from 'graphql-tag';
+import SingleProduct from '../../components/SingleProduct';
 
-const SINGLE_ITEM_QUERY = gql`
-  query SINGLE_ITEM_QUERY($id: ID!) {
-    Product(where: { id: $id }) {
-      name
-      description
-      price
-    }
-  }
-`;
-
-export default function SingleProduct({ query }) {
-  const { data, loading, error } = useQuery(SINGLE_ITEM_QUERY, {
-    variables: { id: query.id },
-  });
-  if (loading) return <p>Loading...</p>;
-  console.log({ data, loading, error });
-
-  return (
-    <div>
-      <p>Hello, I'm the single product page</p>
-      <p>DATA: {data.Product.name}</p>
-    </div>
-  );
+export default function SingleProductPage({ query }) {
+  return <SingleProduct id={query.id} />;
 }
