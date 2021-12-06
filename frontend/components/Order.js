bf3c01fa2245fd42c1646c46be00cc392ce0197a
Cletus Dias <cletus6@gmail.com>
2021-12-05 19:55:28 -0500

    Fixed order styling and removed child comp

--- frontend/components/Order.js
+++ frontend/components/Order.js
@@ -1,18 +1,23 @@
-import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client';
+import Head from 'next/head';
 import OrderStyles from './styles/OrderStyles';
-import OrderItem from './OrderItem';
+import formatMoney from '../lib/formatMoney';
+import OrderItemStyles from './styles/OrderItemStyles';
 
 const SINGLE_ORDER_QUERY = gql`
   query SINGLE_ORDER_QUERY($id: ID!) {
-    allOrders(where: { id: $id }) {
+    Order(where: { id: $id }) {
+      id
       total
       user {
         name
         email
       }
       items {
+        id
         name
+        description
         quantity
         price
         photo {
@@ -33,12 +38,47 @@ export default function Order({ id }) {
   });
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
-  console.log(data);
+  //   console.log(data);
+  const { Order: order } = data;
   return (
     <OrderStyles>
-      {data?.allOrders.map((order) => (
-        <OrderItem key={order.id} order={order} />
-      ))}
+      <Head>
+        <title>{order.id}</title>
+      </Head>
+      <p>
+        <span>Order Id:</span>
+        <span>{order.id}</span>
+      </p>
+      <p>
+        <span>Charge:</span>
+        <span>{order.charge}</span>
+      </p>
+      <p>
+        <span>Order Total:</span>
+        <span>{formatMoney(order.total)}</span>
+      </p>
+      <p>
+        <span>Total Items:</span>
+        <span>{order.items.length}</span>
+      </p>
+      <div className="items">
+        {order.items.map((item) => (
+          <div className="order-item" key={item.id}>
+            <img
+              src={item?.photo?.image?.publicUrlTransformed}
+              alt={item.photo.altText}
+            />
+            <div className="item-details">
+              <h2>{item.name}</h2>
+              <p>
+                {item.quantity} &times; {formatMoney(item.price)} Ea.
+              </p>
+              <p>Subtotal: {formatMoney(item.price * item.quantity)}</p>
+              <p>Description: {item.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </OrderStyles>
   );
 }
