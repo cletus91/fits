edffb3ab4fd39c140a61f195d250408bf9b2846e
Cletus Dias <cletus6@gmail.com>
2021-12-08 21:14:22 -0500

    programmatically generate permissions functions

--- backend/access.ts
+++ backend/access.ts
@@ -1,5 +1,21 @@
+import { permissionsList } from './schemas/fields';
 import { ListAccessArgs } from './types';
 
 export function isSignedIn({ session }: ListAccessArgs) {
   return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }) {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+console.log('generatedPermission', generatedPermissions);
+
+export const permission = {
+  ...generatedPermissions,
+};
